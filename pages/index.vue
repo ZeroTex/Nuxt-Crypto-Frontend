3e6428fa71ca6f31d52caf58ab3197901410b220
zerotex <zerotex@mail.com>
2020-09-12 17:15:44 -0500

    staging with new PriceCard component

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,17 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        nuxt-crypto-prices
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <PriceCard />
   </div>
 </template>
 
 <script>
-export default {}
+import PriceCard from '../components/PriceCard.vue'
+
+export default {
+  component: {
+    PriceCard
+  }
+}
 </script>
 
 <style>
